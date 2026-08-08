bd0002db8adc0f9ab67f26382bf795e47718ee5b
Ayush022007 <singhayush89076@gmail.com>
2026-08-08 16:00:27 +0530

    new

--- first.java
+++ first.java
@@ -1,6 +1,34 @@
+// program to print number in increase or decrease order using reursion
+
 import  java.util.*;
 public class first{
+
+    //decresing method
+    public static void printdec(int n){
+        if(n==1){
+             System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printdec(n-1);
+    }
+    //increasing method
+    public static void printinc(int n) {
+        if(n==1000){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+         printinc(n+1);
+        
+    }
     public static void main(String args[]){
-       System.out.print("hello world"); 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number according to print : ");
+        int n = sc.nextInt();
+     
+        // printdec(n);
+        printinc(n);
+        
     }
 }
